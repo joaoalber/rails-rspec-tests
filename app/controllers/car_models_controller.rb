0ac59d663669ede93dd43e500b67e35d38d88203
Joao Alberto <jooalberto1@gmail.com>
2020-01-09 20:17:55 -0300

    Car model refatorado com before_actions

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -1,37 +1,30 @@
 class CarModelsController < ApplicationController
+    before_action :load_car_categories, :load_manufacturers, only: [:new, :edit]
+    before_action :load_car_model, only: [:update, :show, :edit]
+
+    def show
+    end
+
+    def edit
+    end
+
     def index
         @car_models = CarModel.all
     end
 
-    def show
-        @car_model = CarModel.find(params[:id])
-    end
-
     def new
         @car_model = CarModel.new
-        #@car_categories = CarCategory.all
-        #@manufacturers = Manufacturer.all
     end
 
     def create
         @car_model = CarModel.new(car_model_params)
-        if @car_model.save
-            redirect_to @car_model
-        else    
-            render :new
-        end
-    end
-
-    def edit
-        @car_model = CarModel.find(params[:id])
-        #@car_categories = CarCategory.all
-        #@manufacturers = Manufacturer.all
+        return redirect_to @car_model if @car_model.save
+        load_manufacturers
+        load_car_categories
+        render :new
     end
 
     def update
-        @car_model = CarModel.find(params[:id])
-        #@car_categories = CarCategory.all
-        #@manufacturers = Manufacturer.all
         @car_model.update(car_model_params)
         redirect_to @car_model
     end
@@ -39,6 +32,20 @@ class CarModelsController < ApplicationController
     private
 
     def car_model_params
-        params.require(:car_model).permit(:name, :year, :manufacturer_id, :motorization, :car_category_id, :fuel_type)
+        params.require(:car_model).permit(:name, :year, :manufacturer_id, :motorization, 
+                                          :car_category_id, :fuel_type)
     end
+
+    def load_car_categories
+        @car_categories = CarCategory.all
+    end
+
+    def load_manufacturers
+        @manufacturers = Manufacturer.all
+    end
+
+    def load_car_model
+        @car_model = CarModel.find(params[:id])
+    end
+    
 end
