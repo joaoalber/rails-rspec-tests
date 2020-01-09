e9cc898d982f13165c8a285e44e080b988c4a617
Joao Alberto <jooalberto1@gmail.com>
2020-01-09 20:30:33 -0300

    Subsidiary refatorado com before_action

--- app/controllers/subsidiaries_controller.rb
+++ app/controllers/subsidiaries_controller.rb
@@ -1,46 +1,44 @@
 class SubsidiariesController < ApplicationController
+    before_action :load_subsidiary, only: [:update, :show, :edit, :destroy]
+
     def index
         @subsidiaries = Subsidiary.all
     end
 
     def show
-        @subsidiary = Subsidiary.find(params[:id])
     end
 
     def new
         @subsidiary = Subsidiary.new
     end
 
-    def edit
-        @subsidiary = Subsidiary.find(params[:id])
+    def edit 
     end
 
     def create
         @subsidiary = Subsidiary.new(subsidiary_params)
-        if @subsidiary.save
-            redirect_to @subsidiary
-        else    
-            render :new
-        end
+        return redirect_to @subsidiary, notice: 'Filial cadastrada com sucesso' if @subsidiary.save
+        render :new
     end
 
     def update
-        @subsidiary = Subsidiary.find(params[:id])
         @subsidiary.update(subsidiary_params)
         redirect_to @subsidiary
     end
 
     def destroy
-        @subsidiary = Subsidiary.find(params[:id])
         @subsidiary.destroy!
-        flash[:notice] = 'Filial deletada com sucesso'
-        redirect_to root_path
+        redirect_to root_path, notice: 'Filial deletada com sucesso'
     end
 
     private
 
     def subsidiary_params
         params.require(:subsidiary).permit(:name, :cnpj, :address)
+    end
+
+    def load_subsidiary
+        @subsidiary = Subsidiary.find(params[:id])
     end
 
 end
