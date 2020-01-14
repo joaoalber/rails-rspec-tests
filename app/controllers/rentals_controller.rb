561ae24bcb509bf52f7d79ea918581116f8f7dcc
Joao Alberto <jooalberto1@gmail.com>
2020-01-14 20:11:40 -0300

    Locação com busca sendo realizada

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -7,10 +7,7 @@ class RentalsController < ApplicationController
 	end
 
 	def search
-		 search = params.require(:q)
-		 @rentals = Rental.where(code: "#{search}").to_a
-		 @car_category = CarCategory.find(@rentals.first.car_category_id)
-		 @client = Client.find(@rentals.first.client_id)
-		 render :search
+		 @search = params.require(:q)
+		 @rentals = Rental.where('code LIKE ?', "%#{@search.upcase}%")
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :manufacturers, :subsidiaries, :car_categories, :car_models, :clients, :rentals
-  get 'search', to: 'rentals#search'
+  resources :manufacturers, :subsidiaries, :car_categories, :car_models, :clients
+  resources :rentals do
+    get 'search', on: :collection
+  end
 end
