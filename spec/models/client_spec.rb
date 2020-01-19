d55ef9640b6bb77e72e48a5b015297225a0a1b79
Joao Alberto <jooalberto1@gmail.com>
2020-01-19 15:01:51 -0300

    Teste unitário 'identificação do cliente' feito

---
+++ spec/models/client_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Client, type: :model do
+	describe '.identification' do
+    it 'should generate a identification' do
+			client = Client.create(cpf: "745.291.293-20", name: 'John Doe', email: 'johndoe@gmail.com')
+
+      result = client.identification
+
+      expect(result).to eq "745.291.293-20 - John Doe"
+    end
+  end
+
+end
