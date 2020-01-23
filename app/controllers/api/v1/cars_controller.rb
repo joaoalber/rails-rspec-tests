ff5058da066023b44fb83b46e54881859ac8aefd
Joao Alberto <jooalberto1@gmail.com>
2020-01-22 21:32:09 -0300

    Operador ternário refatorado

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -6,13 +6,13 @@ class Api::V1::CarsController < Api::V1::ApiController
 	end
 
 	def index
-		return render json: @car, status: 200 if @car = Car.all.exists? ? Car.all : nil
+		return render json: @car, status: 200 if @car = Car.all.exists? ? Car.all : false
 
 		head 404
 	end
 	
 	def create
-		return render json: @car, status: 201 if @car = Car.create(car_params).persisted? ? Car.create(car_params) : nil
+		return render json: @car, status: 201 if @car = Car.create(car_params).persisted? ? Car.create(car_params) : false
 		
 		head 412
 	end
